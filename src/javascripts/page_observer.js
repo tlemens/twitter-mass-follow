919e0397f71ee0af0b0ffcf2d00e934fda21564d
Clemens Teichmann <clemens_t@web.de>
2016-12-27 13:00:54 +0100

    Update selector to work with updated Twitter html class

--- src/javascripts/page_observer.js
+++ src/javascripts/page_observer.js
@@ -9,7 +9,8 @@ function observe(element, callback) {
 
 function observeTweetReactions(callback) {
   let element = document.getElementById('activity-popup-dialog')
-  let contentEl = element.getElementsByClassName('activity-content')[0]
+  let contentEl = element.getElementsByClassName('activity-popup-dialog-users')[0]
+  console.log(contentEl)
   let initial = true
   observe(element, function(mutation) {
     if ( element.getElementsByClassName('account').length && initial ) {
